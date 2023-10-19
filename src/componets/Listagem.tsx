13baf4a5572edf881a4518ae6df5cf1140d5f355
twcsefy <mariasefora87@gmail.com>
2023-10-19 10:19:41 -0300

    Committtdtststsytchgasdjg

--- src/componets/Listagem.tsx
+++ src/componets/Listagem.tsx
@@ -1,22 +1,56 @@
-import Reacts, {
+import React, {
     Component, useState,
     ChangeEvent, FormEvent, useEffect
-} from 'react' ;
+} from 'react';
 
 import styles from "../App.module.css";
 import { CadastroInterface } from '../interfaces/CadastroInterface';
 import axios from 'axios';
+
 const Listagem = () => {
 
+
     const [usuarios, setUsuarios] = useState<CadastroInterface[]>([]);
+    const [pesquisa, setPesquisa] = useState<string>('');
     const [error, setError] = useState("");
 
+    const handleState = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.name === "pesquisa") {
+            setPesquisa(e.target.value);
+        }
+    }
+
+    const buscar = (e: FormEvent) => {
+        e.preventDefault();
+
+        async function fetchData() {
+            try {
+
+                const response = await axios.post('http://10.137.9.134:8000/api/findNome',
+                    { nome: pesquisa },
+                    {
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-Type": "application/json"
+                        }
+                    }).then(function (response) {
+                        setUsuarios(response.data.data);
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchData();
+    }
+  
     useEffect(() => {
-        async function fetchData(){
-            try{
-                const response = await axios.get('http://10.137.9.132:8000/api/find');
+        async function fetchData() {
+            try {
+                const response = await axios.get('http://10.137.9.134:8000/api/find');
                 setUsuarios(response.data.data);
-            }catch(error){
+            } catch (error) {
                 setError("Ocorreu um erro");
                 console.log(error);
             }
@@ -28,31 +62,53 @@ const Listagem = () => {
         <div>
             <main className={styles.main}>
                 <div className='container'>
+
+                    <div className='col-md mb-3'>
+                        <div className='card'>
+                            <div className='card-body'>
+                                <h5 className='card-title'>Pesquisar</h5>
+                                <form onSubmit={buscar} className='row' >
+                                    <div className='col-10'>
+                                        <input type="text" name="pesquisa"
+                                            className='form-control' onChange={handleState} />
+                                    </div>
+                                    <div className='col-1'>
+                                        <button type='submit'
+                                            className='btn btn-success'>Pesquisar</button>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
                     <div className='card'>
                         <div className='card-body'>
                             <h5 className='card-title'>Listagem de Usuários</h5>
                             <table className='table table-hover'>
                                 <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nome</th>
-                                    <th>CPF</th>
-                                    <th>E-mail</th>
-                                    <th>Ações</th>
-                                </tr>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nome</th>
+                                        <th>CPF</th>
+                                        <th>E-mail</th>
+                                        <th>Ações</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
                                     {usuarios.map(usuario => (
-                                    <tr key={usuario.id}>
-                                        <td>{usuario.id}</td>
-                                        <td>{usuario.nome}</td>
-                                        <td>{usuario.cpf}</td>
-                                        <td>{usuario.email}</td>
-                                        <td>
-                                            <a href="#" className='btn btn-primary btn-sm'>Editar</a>
-                                            <a href="#"className='btn btn-danger btn-sm'>Excluir</a>
-                                        </td>
-                                    </tr>
+                                        <tr key={usuario.id}>
+                                            <td>{usuario.id}</td>
+                                            <td>{usuario.nome}</td>
+                                            <td>{usuario.cpf}</td>
+                                            <td>{usuario.email}</td>
+                                            <td>
+                                                <a href="#" className='btn btn-primary btn-sm'>Editar</a>
+                                                <a href="#" className='btn btn-danger btn-sm'>Excluir</a>
+                                            </td>
+                                        </tr>
                                     ))}
                                 </tbody>
                             </table>
